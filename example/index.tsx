ffde64cfd20ed734d32b65eed4274ffd5327bbbe
u4bi <rootcode10@gmail.com>
2019-12-27 23:35:18 +0900

    feat: Add default props to Payple component.

--- example/index.tsx
+++ example/index.tsx
@@ -2,8 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Payple from '../src/index'
 
+const key = {
+    cst_id: 'test',
+    cust_key: 'abcd1234567890',
+    auth_url: 'https://testcpay.payple.kr/php/auth.php'
+}
+
+const form = {
+    name: '하얀눈',
+    amount: 11000,
+    buyer_no: `payple_${ new Date().getMilliseconds() }`,
+    buyer_email: 'example@example.com',
+    buyer_name : '김테스트',
+    buyer_tel: '01012345678'
+}
+
+const data = {
+    PCD_CST_ID: key.cst_id,
+    PCD_CUST_KEY: key.cust_key,
+    PCD_AUTH_URL: key.auth_url,
+    PCD_CPAY_VER: '1.0.1',
+    PCD_PAY_TYPE: 'transfer',
+    PCD_PAY_WORK: 'PAY',
+    PCD_PAY_GOODS: form.name,
+    PCD_PAY_TOTAL: form.amount,
+    PCD_PAYER_NO: form.buyer_no,
+    PCD_PAY_ISTAX: 'N',
+    PCD_PAYER_EMAIL: form.buyer_email,
+    PCD_PAYER_NAME: form.buyer_name,
+    PCD_PAYER_HP: form.buyer_tel
+}
+
 const root = document.createElement('div')
 
 document.body.append(root)
 
-ReactDOM.render(<Payple text='test' />, root)
+ReactDOM.render(<Payple data={ data } onCallback= { (e) => console.log(e) }>
+    <button>
+        test
+    </button>
+</Payple>, root)
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,58 @@
-import React, { FC } from 'react'
+import React, { FC, cloneElement } from 'react'
 
-type Props = {
-    text: string
+type Data = {
+    PCD_CST_ID: string,
+    PCD_CUST_KEY: string,
+    PCD_AUTH_URL: string,
+    PCD_CPAY_VER: string,
+    PCD_PAY_TYPE: string,
+    PCD_PAY_WORK: string,
+    PCD_PAYER_NO: string,
+    PCD_AUTH_KEY?: string,
+    PCD_CARD_VER?: string,
+    PCD_PAYER_ID?: string,
+    PCD_PAY_GOODS: string,
+    PCD_RST_URL?: string,
+    PCD_PAYER_NAME?: string,
+    PCD_PAYER_HP?: string,
+    PCD_PAYER_EMAIL?: string,
+    PCD_PAY_TOTAL?: number,
+    PCD_PAY_OID?: string,
+    PCD_REGULER_FLAG?: string,
+    PCD_PAY_YEAR?: string,
+    PCD_PAY_MONTH?: string,
+    PCD_TAXSAVE_FLAG?: string,
+    PCD_SIMPLE_FLAG?: string,
+    PCD_PAYER_AUTHTYPE?: string,
+    PCD_HTTP_REFERER?: string
 }
 
-const Payple: FC<Props> = ({ text }) => (
-    <div>
-        { text }
-    </div>
-)
+type OnCallback = (e: any) => void
+
+type Props = {
+    data: Data,
+    onCallback: OnCallback,
+    children: any
+}
+
+const Payple: FC<Props> = ({ data, onCallback, children }) => {
+
+    const run = () : void => {
+
+        const { PCD_AUTH_URL, PCD_CST_ID, PCD_CUST_KEY } = data
+
+        console.log(PCD_AUTH_URL, PCD_CST_ID, PCD_CUST_KEY)
+
+    }
+
+    return (
+        <div>
+
+            { cloneElement(children, { onClick: run })}
+
+        </div>
+    )
+
+}
 
 export default Payple
